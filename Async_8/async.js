59d5ccf6ffd140d1a21ec2e27ebe0f6830fe9925
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-04-13 20:02:18 +0530

    Studied about setInterval & clearInterval

--- Async_8/async.js
+++ Async_8/async.js
@@ -1,19 +1,19 @@
-setTimeout(function(){
-    console.log("Mr_Shamra");
-}, 2000) //Executes once after the given time i.e. 2seconds
+// setTimeout(function(){
+//     console.log("Mr_Shamra");
+// }, 2000) //Executes once after the given time i.e. 2seconds
 
 
-const Don = () =>{
-    console.log(`This is an external Function`);
-    document.querySelector('h1').innerHTML = 'This is My Learnings';
-}
+// const Don = () =>{
+//     console.log(`This is an external Function`);
+//     document.querySelector('h1').innerHTML = 'This is My Learnings';
+// }
 
-const changeMe = setTimeout(Don, 2000);
+// const changeMe = setTimeout(Don, 2000);
 
-document.querySelector('button').addEventListener('click', ()=>{
-    clearTimeout(changeMe);
-    console.log('stopped');
-})
+// document.querySelector('#stop').addEventListener('click', ()=>{
+//     clearTimeout(changeMe);
+//     console.log('stopped');
+// })
 
 
 // -------------------------------------------------------------------------------------------------------
@@ -23,4 +23,17 @@ document.querySelector('button').addEventListener('click', ()=>{
 //     console.log("Mr_Sharma - Interval");
 // }, 2000); //Executes endlessly with the given  interval i.e. 2seconds
 
- 
+ const sayDate = (str) =>{
+    console.log(`${str} Mr_Sharma`, Date.now());
+ }
+ let intervalId = null;
+
+document.querySelector('#start').addEventListener('click', ()=>{
+    console.log('Interval Started');
+    intervalId = setInterval(sayDate, 2000, "Hey");  // we can also pass arguments with setInterval;
+})
+
+document.querySelector('#stop').addEventListener('click', ()=>{
+    clearInterval(intervalId); //The setInterval gets stopped
+    console.log('Interval stopped');
+})
